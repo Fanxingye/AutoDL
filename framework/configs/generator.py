512abf9ff685c012a196f70105ed7a93eb6dfb01
stan.xu <619677632@qq.com>
2021-06-19 18:45:22 +0800

    finish update best configuration pipeline and add debug model

--- framework/configs/generator.py
+++ framework/configs/generator.py
@@ -1,7 +1,9 @@
+import json
 import os
 
 import autogluon.core as ag
 import pandas as pd
+import yaml
 
 from utils.constant import Constant
 
@@ -37,8 +39,8 @@ class ConfigGenerator:
     # TODO
     # parser mongodb
     # parser csv to config
-    def __init__(self, dataset=None, time_limit=None, device_limit=1):
-        self.dataset = dataset
+    def __init__(self, dataset_name=None, time_limit=None, device_limit=1):
+        self.dataset_name = dataset_name
         self.time_limit = time_limit
         self.device_limit = device_limit
 
@@ -111,7 +113,7 @@ class ConfigGenerator:
         df = pd.read_csv(Constant.DATASET_CONFIGURATION_CSV)
         # TODO
         # 1 to 1 dataset_name
-        dataset_config = df.loc[df["dataset_name"] == self.dataset]
+        dataset_config = df.loc[df["dataset_name"] == self.dataset_name]
         if dataset_config.size > 0:
             return dataset_config.to_dict(
                 orient='records')[0], dataset_config["framework"].values[0]
@@ -131,7 +133,7 @@ class ConfigGenerator:
         for key, value in config.items():
             if self.SPACE_FUNCTION_DICT.__contains__(key):
                 config[key] = self.SPACE_FUNCTION_DICT[key](self, value,
-                                                             framework)
+                                                            framework)
         return config
 
     def generate_config(self):
@@ -140,22 +142,70 @@ class ConfigGenerator:
         :return: configuration dictionary
                  framework string
         '''
-        selected_config, framework = self.select_dataset_config()
+        selected_config, self.framework = self.select_dataset_config()
 
-        if framework == Constant.AUTO_GLUON:
+        if self.framework == Constant.AUTO_GLUON:
             config = DefaultConfig.autogluon
             # TODO
             # time estim
             selected_config = self.generate_hpo_space(selected_config,
-                                                       framework)
+                                                      self.framework)
             config["hyperparameters"].update(selected_config)
             config["ngpus_per_trial"] = self.device_limit
             if self.time_limit:
                 config["time_limit"] = self.time_limit
-            return config, framework
+            return config, self.framework
 
-    def update_config_csv(self, config):
-        config_pd = pd.read_csv(Constant.DATASET_CONFIGURATION_CSV)
+    def update_config_csv(self, checkpoint_dir):
+        def find_best_autogluon_config():
+            _BEST_CHECKPOINT_FILE = 'best_checkpoint.pkl'
+            valid_summary_file = 'fit_summary_img_cls.ag'
+            _BEST_CONFIG_FILE = 'config.yaml'
+            best_checkpoint = ''
+            best_config = ''
+            best_acc = -1
+            val_result = {}
+            for root, dirs, files in os.walk(checkpoint_dir):
+                for trial_name in dirs:
+                    if trial_name.startswith('.trial_'):
+                        trial_dir = os.path.join(root, trial_name)
+                        try:
+                            with open(os.path.join(trial_dir, valid_summary_file), 'r') as f:
+                                val_result = json.load(f)
+                                acc = val_result.get('valid_acc', -1)
+                                print("=" * 30, "Check history trials results: ", trial_dir, acc)
+                                if acc > best_acc and os.path.isfile(os.path.join(trial_dir, _BEST_CHECKPOINT_FILE)):
+                                    best_checkpoint = os.path.join(trial_dir, _BEST_CHECKPOINT_FILE)
+                                    best_config = os.path.join(trial_dir, _BEST_CONFIG_FILE)
+                                    print("=" * 30, "Find a Better checkpoint : ", best_checkpoint)
+                                    print("=" * 30, "Find a Better config : ", best_config)
+                                    best_acc = acc
+                        except Exception as e:
+                            print(e)
+            config_dict = {}
+            if os.path.isfile(best_config):
+                with open(best_config, 'r') as f:
+                    config_yaml = yaml.load(f, Loader=yaml.FullLoader)
+                    config_dict["model"] = config_yaml.get('img_cls').get('model')
+                    config_dict["batch_size"] = config_yaml.get('train').get('batch_size')
+                    config_dict["epochs"] = config_yaml.get('train').get('epochs')
+                    config_dict["lr"] = config_yaml.get('train').get('lr')
+                    config_dict["momentum"] = config_yaml.get('train').get('momentum')
+                    config_dict["wd"] = config_yaml.get('train').get('wd')
+                    # input_size = config_yaml.get('train').get('input_size')
+                    config_dict["early_stop_patience"] = config_yaml.get('train').get('early_stop_patience')
+                    config_dict["total_time"] = val_result.get("total_time")
+                    config_dict["dataset_name"] = self.dataset_name
+                    config_dict["framework"] = self.framework
+
+            return config_dict
+
+        if self.framework == Constant.AUTO_GLUON:
+            best_config = find_best_autogluon_config()
+            if best_config:
+                config_pd = pd.read_csv(Constant.DATASET_CONFIGURATION_CSV)
+                config_pd.append(best_config, ignore_index=True)
+                config_pd.to_csv(Constant.DATASET_CONFIGURATION_CSV, index=False)
 
     def yaml_to_csv(self, yaml_path, is_update=False):
         pass
--- framework/utils/utils.py
+++ framework/utils/utils.py
@@ -1,6 +1,7 @@
-import os
-import yaml
 import json
+import os
+
+import yaml
 
 
 def load_yaml(file_path):
@@ -13,3 +14,49 @@ def load_json(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
         result = json.load(f)
     return result
+
+
+def find_best_model_loop(checkpoint_dir):
+    _BEST_CHECKPOINT_FILE = 'best_checkpoint.pkl'
+    valid_summary_file = 'fit_summary_img_cls.ag'
+    _BEST_CONFIG_FILE = 'config.yaml'
+    best_checkpoint = ''
+    best_config = ''
+    best_acc = -1
+    result = {}
+    for root, dirs, files in os.walk(checkpoint_dir):
+        for trial_name in dirs:
+            if trial_name.startswith('.trial_'):
+                trial_dir = os.path.join(root, trial_name)
+                try:
+                    with open(os.path.join(trial_dir, valid_summary_file), 'r') as f:
+                        result = json.load(f)
+                        acc = result.get('valid_acc', -1)
+                        print("=" * 30, "Check history trials results: ", trial_dir, acc)
+                        if acc > best_acc and os.path.isfile(os.path.join(trial_dir, _BEST_CHECKPOINT_FILE)):
+                            best_checkpoint = os.path.join(trial_dir, _BEST_CHECKPOINT_FILE)
+                            best_config = os.path.join(trial_dir, _BEST_CONFIG_FILE)
+                            print("=" * 30, "Find a Better checkpoint : ", best_checkpoint)
+                            print("=" * 30, "Find a Better config : ", best_config)
+                            best_acc = acc
+                except Exception as e:
+                    print(e)
+    return best_checkpoint, best_config, result
+
+
+def parse_config(config_file):
+    with open(config_file, 'r') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    model = config.get('img_cls').get('model')
+    batch_size = config.get('train').get('batch_size')
+    epochs = config.get('train').get('epochs')
+    learning_rate = config.get('train').get('lr')
+    momentum = config.get('train').get('momentum')
+    wd = config.get('train').get('wd')
+    input_size = config.get('train').get('input_size')
+
+    return model, batch_size, epochs, learning_rate, momentum, wd, input_size
+
+if __name__ == '__main__':
+    find_best_model_loop('/home/robin/jianzh/automl/autodl/benchmark/hymenoptera')
