a034a378c0657c163ed23ba15124c615300f08c9
stan.xu <619677632@qq.com>
2021-06-21 21:56:28 +0800

    update dataaug code , add clear method , configuration update

--- framework/data/augmentation/data_aug.py
+++ framework/data/augmentation/data_aug.py
@@ -1,16 +1,27 @@
+import os
+import random
+import shutil
+import string
+
 import albumentations as A
 import cv2
-import os
-import math
-import string
-import random
 
 
 class DataAug:
+    '''
+        - dog-vs-cat
+        - split
+            - train
+            - train_dataaug
+            - test
+            - val
+    '''
     def __init__(self, task_config, similar_datasets, engineer_feature, aug_folder_name="train_dataaug"):
         self.task_config = task_config
         self.engineer_feature = engineer_feature
-        self.aug_folder_name = aug_folder_name
+        self.dataset_path = self.task_config["data_path"]
+        self.split_train_path = os.path.join(self.dataset_path, "split", "train")
+        self.aug_dir = os.path.join(self.dataset_path, "split", aug_folder_name)
         self.similar_datasets = similar_datasets
 
     def generate_aug_dataset(self, level=2):
@@ -18,31 +29,34 @@ class DataAug:
         #     return cache
         # return aug dataset path
         print(self.task_config)
-        dataset_path=self.task_config["data_path"]
-        path = f'{dataset_path}/split/'
-        size = len(os.listdir(f'{path}/train/'))
         index = 1
-        for i in os.listdir(f'{path}/train/'):
-            index+=1
-            for j in os.listdir(f'{path}/train/{i}'):
-                image = cv2.imread(f'{path}/train/{i}/{j}')
+        for image_label in os.listdir(self.split_train_path):
+            index += 1
+            image_label_dir = os.path.join(self.split_train_path, image_label)
+            for image_name in os.listdir(image_label_dir):
+                image = cv2.imread(os.path.join(image_label_dir, image_name))
                 for x in range(level):
                     transform = A.Compose([
-                    A.Rotate(limit=20,p=1),
-                    A.OneOf([
-                        #A.RGBShift(p=0.3),
-                        A.RandomBrightnessContrast(p=0.3),
-                    ],p=1),
-                    A.Flip(p=0.3),
-                    A.RandomResizedCrop(image.shape[0],image.shape[1],scale=(0.75, 1.0), ratio=(0.75, 1.3333333333333333), p=0.3),
+                        A.Rotate(limit=20, p=1),
+                        A.OneOf([
+                            # A.RGBShift(p=0.3),
+                            A.RandomBrightnessContrast(p=0.3),
+                        ], p=1),
+                        A.Flip(p=0.3),
+                        A.RandomResizedCrop(image.shape[0], image.shape[1], scale=(0.75, 1.0),
+                                            ratio=(0.75, 1.3333333333333333), p=0.3),
                     ])
                     transformed = transform(image=image)
                     transformed_image = transformed["image"]
-    #                 transformed_image=RandomErasing(image) 
-                    save_path = f'{path}/{self.aug_folder_name}/{i}/'
-                    if not os.path.exists(save_path):
-                        os.makedirs(save_path)
-                    save_name = f'aug{x}_'+''.join(random.sample(string.ascii_letters + string.digits, 32))+'.jpg'
-                    cv2.imwrite(os.path.join(f'{save_path}'+save_name),transformed_image)
-                cv2.imwrite(f'{path}/{self.aug_folder_name}/{i}/{j}',image)  #copy the ori image
-        return f'{path}/{self.aug_folder_name}/'
+                    #                 transformed_image=RandomErasing(image)
+                    aug_path = os.path.join(self.aug_dir, image_label)
+                    if not os.path.exists(aug_path):
+                        os.makedirs(aug_path)
+                    save_name = f'aug{x}_' + ''.join(random.sample(string.ascii_letters + string.digits, 32)) + '.jpg'
+                    cv2.imwrite(os.path.join(aug_path, save_name), transformed_image)
+                shutil.copy(os.path.join(image_label_dir, image_name),
+                            os.path.join(aug_path, image_name))  # copy the ori image
+        return self.aug_dir
+
+    def clear(self):
+        shutil.rmtree(self.aug_dir)
