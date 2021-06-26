52fe96f44f0a9e3bb3a21acaf4c580b017c45adf
stan.xu <619677632@qq.com>
2021-06-26 18:44:24 +0800

    update ocr  cls model

--- framework/expert/test_ocr_classifier.py
+++ framework/expert/test_ocr_classifier.py
@@ -1,5 +1,6 @@
+from ocr_bert_classifier import OCRBertClassifier
+
 from data.utils.data_split import DataSplit
-from expert.ocr_bert_classifier import OCRBertClassifier
 from utils import utils
 
 yaml_file = "../config.yaml"
@@ -9,13 +10,15 @@ task_config = utils.load_yaml(yaml_file)
 
 train_dataset, val_dataset, test_dataset = DataSplit(
     task_config).run_data_split()
-
+# train_dataset, val_dataset, test_dataset=task_config["data_path"]+"train",task_config["data_path"]+"val",task_config["data_path"]+"test"
 ocr_bert = OCRBertClassifier(task_config, train_dataset, val_dataset, test_dataset)
 
 
-def test_bert_train():
-    ocr_bert.fit()
-
+def test_bert_fit():
+    ocr_bert.generate_bert_dataset()
+def test_bert_ensemble_eval():
+    ocr_bert.ensemble_eval()
 
 if __name__ == '__main__':
-    test_bert_train()
+    test_bert_ensemble_eval()
+
