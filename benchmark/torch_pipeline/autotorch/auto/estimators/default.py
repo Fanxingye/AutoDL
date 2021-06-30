a7fa67cc95b087c5d673de3ed1b372e31a489fb1
jianzheng.nie <jianzhnie@126.com>
2021-06-30 16:46:41 +0800

    add torch imageestimator

--- benchmark/torch_pipeline/autotorch/auto/estimators/default.py
+++ benchmark/torch_pipeline/autotorch/auto/estimators/default.py
@@ -6,7 +6,7 @@ from autocfg import dataclass, field
 
 @dataclass
 class ImageClassification:
-    model : str = 'resnet18'
+    model_name : str = 'resnet18'
     use_pretrained : bool = True
     use_gn : bool = False
     batch_norm : bool = False
@@ -25,21 +25,26 @@ class TrainCfg:
     lr_schedule_mode : str = 'step'  # learning rate scheduler mode. options are step, poly and cosine
     warmup_lr : float = 0.0  # starting warmup learning rate.
     warmup_epochs : int = 0  # number of warmup epochs
-    num_workers : int = 4
-    wd : float = 0.0001
+    num_workers : int = 0
+    weight_decay : float = 0.0001
     momentum : float = 0.9
+    nesterov : bool = False
     dtype : str = 'float32'
     input_size : int = 224
     crop_ratio : float = 0.875
-    data_dir : str = '~/.mxnet/datasets/imagenet'
-    mixup : bool = False
+    data_augment : str = ''
+    data_dir : str = ''
     no_wd : bool = False
     label_smoothing : bool = False
     resume_epoch : int = 0
+    mixup : bool = False
     mixup_alpha : float = 0.2
     mixup_off_epoch : int = 0
     log_interval : int = 50
     mode : str = ''
+    amp: bool = False
+    static_loss_scale : float = 1.0
+    dynamic_loss_scale : bool = False
     start_epoch : int = 0
     transfer_lr_mult : float = 0.01  # reduce the backbone lr_mult to avoid quickly destroying the features
     output_lr_mult : float = 0.1  # the learning rate multiplier for last fc layer if trained with transfer learning
@@ -51,15 +56,21 @@ class TrainCfg:
 
 @dataclass
 class ValidCfg:
-    batch_size : int = 128
+    batch_size : int = 16
     num_workers : int = 4
 
-
 @dataclass
-class ImageClassificationCfg:
-    img_cls : ImageClassification = field(default_factory=ImageClassification)
-    train : TrainCfg = field(default_factory=TrainCfg)
-    valid : ValidCfg = field(default_factory=ValidCfg)
+class RunTimeCfg:
     gpus : Union[Tuple, list] = (0, )  # gpu individual ids, not necessarily consecutive
     launcher : str = 'pytorch'
     cudnn_benchmark : bool = True
+
+@dataclass
+class ImageClassificationCfg:
+    img_cls: ImageClassification = field(default_factory=ImageClassification)
+    train: TrainCfg = field(default_factory=TrainCfg)
+    valid: ValidCfg = field(default_factory=ValidCfg)
+    gpus:  Union[int, list] = (0)
+    launcher: str = 'none'
+    cudnn_benchmark: bool = True
+    distributed: bool = False
