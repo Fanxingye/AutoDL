222f01489c6d8f8c421a96ed36bd5ec82eb36bf4
yiran <yiran.wu@apulis.com>
2021-07-06 03:15:37 +0000

    rename the file

---
+++ benchmark/torch_pipeline/autotorch/scheduler/__init__.py
@@ -0,0 +1,7 @@
+from .cosine_lr import CosineLRScheduler
+from .linear_lr import LinearLRScheduler
+from .step_lr import StepLRScheduler
+from .exponential_lr import ExponentialLRScheduler
+
+__all__=["CosineLRScheduler", "LinearLRScheduler"
+        "StepLRScheduler", "ExponentialLRScheduler"]
