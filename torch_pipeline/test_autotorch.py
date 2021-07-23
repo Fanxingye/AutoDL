78794356769a57fd7106966e85347fc6cd484c7d
yiran <yiran.wu@apulis.com>
2021-07-23 02:13:42 +0000

    update file for new config

--- torch_pipeline/test_autotorch.py
+++ torch_pipeline/test_autotorch.py
@@ -4,18 +4,18 @@ from autotorch.proxydata import ProxyModel
 
 # train_dataset, _, test_dataset = ImagePredictor.Dataset.from_folders(
 #     'https://autogluon.s3.amazonaws.com/datasets/shopee-iet.zip')
-train_dataset, valid_dataset, test_dataset = ImagePredictor.Dataset.from_folders("/data/AutoML_compete/leafy-vegetable-pests/split/")
+train_dataset = ImagePredictor.Dataset.from_folder("/data/AutoML_compete/leafy-vegetable-pests/train")
 
 predictor = ImagePredictor(log_dir='checkpoint')
 # predictor = predictor.load("/data/autodl/torch_pipeline/checkpoint/bddb5b08/.trial_0/best_checkpoint.pkl")
 # since the original dataset does not provide validation split, the `fit` function splits it randomly with 90/10 ratio
 predictor.fit(
     train_data=train_dataset,
-    tuning_data=train_dataset,
+    # tuning_data=valid_dataset,
     hyperparameters={
-        'model': ag.Categorical('swin_base_patch4_window7_224_in22k'),
-        'batch_size': ag.Categorical(32),
-        'lr': 0.001,
+        'model': ag.Categorical('tf_efficientnetv2_m'),
+        'batch_size': ag.Categorical(8),
+        'lr': 0.005,
         'epochs': 30,
         'ngpus_per_trial': 2,
         'cleanup_disk': False
