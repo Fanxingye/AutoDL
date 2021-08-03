4386b2412fa0d6a96a1249c5bfc05368e82422d0
yiran <yiran.wu@apulis.com>
2021-08-03 06:54:07 +0000

    add files for data similarity

---
+++ torch_pipeline/test_dnn.py
@@ -0,0 +1,15 @@
+from autotorch.features.dnn import DNNFeature
+from types import SimpleNamespace
+
+config = {
+  "name": "classification",
+  "time_limit_sec": "36000",
+  "data_name": "A-Large-Scale-Fish-Dataset1",
+  "data_path": "/data/AutoML_compete/A-Large-Scale-Fish-Dataset/split/train",
+  "device_limit": "8",
+  "device_type": "nvidia"
+}
+
+dnnf = DNNFeature(config, save_to_file=False) 
+top_3 = dnnf.calculate_similarity_topk(3)
+print(top_3)
