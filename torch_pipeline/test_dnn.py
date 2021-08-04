28208b82b0ca09a1a90be89b0ec6e150aba4f3ca
yiran <yiran.wu@apulis.com>
2021-08-04 07:03:04 +0000

    modify file for import package

--- torch_pipeline/test_dnn.py
+++ torch_pipeline/test_dnn.py
@@ -4,7 +4,7 @@ from types import SimpleNamespace
 config = {
   "name": "classification",
   "time_limit_sec": "36000",
-  "data_name": "A-Large-Scale-Fish-Dataset1",
+  "data_name": "A-Large-Scale-Fish-Dataset2",
   "data_path": "/data/AutoML_compete/A-Large-Scale-Fish-Dataset/split/train",
   "device_limit": "8",
   "device_type": "nvidia"
