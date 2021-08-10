98c048cb85e79fff034816347babdaa29d3de3da
jianzheng.nie <jianzhnie@126.com>
2021-08-10 18:43:02 +0800

    add support training speed

--- torch_pipeline/autotorch/training.py
+++ torch_pipeline/autotorch/training.py
@@ -49,7 +49,13 @@ class ModelAndLoss(nn.Module):
             self.model.load_state_dict(state, strict=False)
 
 
-def get_train_step(model, criterion, optimizer, scaler, use_amp=False, batch_size_multiplier=1, top_k=1):
+def get_train_step(model,
+                   criterion,
+                   optimizer,
+                   scaler,
+                   use_amp=False,
+                   batch_size_multiplier=1,
+                   top_k=1):
     def _step(input, target, optimizer_step=True):
         input_var = Variable(input)
         target_var = Variable(target)
@@ -59,7 +65,9 @@ def get_train_step(model, criterion, optimizer, scaler, use_amp=False, batch_siz
             loss = criterion(output, target_var)
 
             loss /= batch_size_multiplier
-            prec1, prec5 = accuracy(output.data, target, topk=(1, min(top_k, 5)))
+            prec1, prec5 = accuracy(output.data,
+                                    target,
+                                    topk=(1, min(top_k, 5)))
             if torch.distributed.is_initialized():
                 reduced_loss = reduce_tensor(loss.data)
                 prec1 = reduce_tensor(prec1)
@@ -81,22 +89,20 @@ def get_train_step(model, criterion, optimizer, scaler, use_amp=False, batch_siz
     return _step
 
 
-def train(
-    train_loader,
-    model,
-    criterion,
-    optimizer,
-    scaler,
-    lr_scheduler,
-    num_class,
-    logger,
-    epoch,
-    timeout_handler,
-    ema=None,
-    use_amp=False,
-    batch_size_multiplier=1,
-    log_interval=1
-):
+def train(train_loader,
+          model,
+          criterion,
+          optimizer,
+          scaler,
+          lr_scheduler,
+          num_class,
+          logger,
+          epoch,
+          timeout_handler,
+          ema=None,
+          use_amp=False,
+          batch_size_multiplier=1,
+          log_interval=1):
     batch_time_m = AverageMeter('Time', ':6.3f')
     data_time_m = AverageMeter('Data', ':6.3f')
     losses_m = AverageMeter('Loss', ':.4e')
@@ -104,21 +110,20 @@ def train(
     top5_m = AverageMeter('Acc@5', ':6.2f')
 
     interrupted = False
-    step = get_train_step(
-        model,
-        criterion,
-        optimizer,
-        scaler=scaler,
-        use_amp=use_amp,
-        batch_size_multiplier=batch_size_multiplier,
-        top_k=num_class
-    )
+    step = get_train_step(model,
+                          criterion,
+                          optimizer,
+                          scaler=scaler,
+                          use_amp=use_amp,
+                          batch_size_multiplier=batch_size_multiplier,
+                          top_k=num_class)
 
     model.train()
     optimizer.zero_grad()
     steps_per_epoch = len(train_loader)
     data_iter = enumerate(train_loader)
     end = time.time()
+    batch_size = 1
     for i, (input, target) in data_iter:
         input = input.cuda()
         target = target.cuda()
@@ -130,7 +135,7 @@ def train(
         optimizer_step = ((i + 1) % batch_size_multiplier) == 0
         loss, prec1, prec5 = step(input, target, optimizer_step=optimizer_step)
         if ema is not None:
-            ema(model, epoch*steps_per_epoch+i)
+            ema(model, epoch * steps_per_epoch + i)
 
         it_time = time.time() - end
         batch_time_m.update(it_time)
@@ -140,13 +145,15 @@ def train(
         top5_m.update(prec5.item(), bs)
 
         end = time.time()
-        if ((i+1) % 20 == 0) and timeout_handler.interrupted:
+        if ((i + 1) % 20 == 0) and timeout_handler.interrupted:
             time.sleep(5)
             interrupted = True
             break
-
+        if i == 1:
+            batch_size = bs
         if (i % log_interval == 0) or (i == steps_per_epoch - 1):
-            if not torch.distributed.is_initialized() or torch.distributed.get_rank() == 0:
+            if not torch.distributed.is_initialized(
+            ) or torch.distributed.get_rank() == 0:
                 learning_rate = optimizer.param_groups[0]["lr"]
                 log_name = 'Train-log'
                 logger.info(
@@ -156,11 +163,18 @@ def train(
                     'Loss: {loss.val:>7.4f} ({loss.avg:>6.4f}) '
                     'Acc@1: {top1.val:>7.4f} ({top1.avg:>7.4f}) '
                     'Acc@5: {top5.val:>7.4f} ({top5.avg:>7.4f}) '
-                    'lr: {lr:>4.6f} '.format(
-                        log_name, epoch+1, i, steps_per_epoch, data_time=data_time_m,
-                        batch_time=batch_time_m,
-                        loss=losses_m, top1=top1_m, top5=top5_m, lr=learning_rate))
-    return interrupted
+                    'lr: {lr:>4.6f} '.format(log_name,
+                                             epoch + 1,
+                                             i,
+                                             steps_per_epoch,
+                                             data_time=data_time_m,
+                                             batch_time=batch_time_m,
+                                             loss=losses_m,
+                                             top1=top1_m,
+                                             top5=top5_m,
+                                             lr=learning_rate))
+
+    return interrupted, losses_m.avg, top1_m.avg / 100.0, top5_m.avg / 100.0, batch_size
 
 
 def get_val_step(model, criterion, use_amp=False, top_k=1):
@@ -172,7 +186,9 @@ def get_val_step(model, criterion, use_amp=False, top_k=1):
             output = model(input_var)
             loss = criterion(output, target_var)
 
-            prec1, prec5 = accuracy(output.data, target, topk=(1, min(5, top_k)))
+            prec1, prec5 = accuracy(output.data,
+                                    target,
+                                    topk=(1, min(5, top_k)))
 
             if torch.distributed.is_initialized():
                 reduced_loss = reduce_tensor(loss.data)
@@ -188,16 +204,14 @@ def get_val_step(model, criterion, use_amp=False, top_k=1):
     return _step
 
 
-def validate(
-    val_loader,
-    model,
-    criterion,
-    num_class,
-    logger,
-    logger_name,
-    use_amp=False,
-    log_interval=10
-):
+def validate(val_loader,
+             model,
+             criterion,
+             num_class,
+             logger,
+             logger_name,
+             use_amp=False,
+             log_interval=10):
     batch_time_m = AverageMeter('Time', ':6.3f')
     data_time_m = AverageMeter('Data', ':6.3f')
     losses_m = AverageMeter('Loss', ':.4e')
@@ -214,7 +228,7 @@ def validate(
     for i, (input, target) in data_iter:
         input = input.cuda()
         target = target.cuda()
-        
+
         bs = input.size(0)
         data_time = time.time() - end
         loss, prec1, prec5 = step(input, target)
@@ -223,12 +237,13 @@ def validate(
 
         batch_time_m.update(it_time)
         data_time_m.update(data_time)
-        losses_m.update(loss.item(),  bs)
+        losses_m.update(loss.item(), bs)
         top1_m.update(prec1.item(), bs)
         top5_m.update(prec5.item(), bs)
 
         if (i % log_interval == 0) or (i == steps_per_epoch - 1):
-            if not torch.distributed.is_initialized() or torch.distributed.get_rank() == 0:
+            if not torch.distributed.is_initialized(
+            ) or torch.distributed.get_rank() == 0:
                 logger.info(
                     '{0}: [{1:>2d}/{2}] '
                     'DataTime: {data_time.val:.3f} ({data_time.avg:.3f}) '
@@ -236,9 +251,14 @@ def validate(
                     'Loss: {loss.val:>7.4f} ({loss.avg:>6.4f}) '
                     'Acc@1: {top1.val:>7.4f} ({top1.avg:>7.4f}) '
                     'Acc@5: {top5.val:>7.4f} ({top5.avg:>7.4f})'.format(
-                        logger_name, i, steps_per_epoch, data_time=data_time_m,
+                        logger_name,
+                        i,
+                        steps_per_epoch,
+                        data_time=data_time_m,
                         batch_time=batch_time_m,
-                        loss=losses_m, top1=top1_m, top5=top5_m))
+                        loss=losses_m,
+                        top1=top1_m,
+                        top5=top5_m))
     return top1_m.avg
 
 
@@ -271,13 +291,14 @@ def train_loop(
 
     if early_stopping_patience > 0:
         epochs_since_improvement = 0
-    
+
     print(f"RUNNING EPOCHS FROM {start_epoch} TO {end_epoch}")
     with TimeoutHandler() as timeout_handler:
         interrupted = False
         for epoch in range(start_epoch, end_epoch):
             if not skip_training:
-                interrupted = train(
+                tic = time.time()
+                interrupted, losses_m, top1_m, top5_m, batch_size = train(
                     train_loader,
                     model,
                     criterion,
@@ -291,8 +312,17 @@ def train_loop(
                     ema=ema,
                     use_amp=use_amp,
                     batch_size_multiplier=batch_size_multiplier,
-                    log_interval=10
-                )
+                    log_interval=10)
+
+            batch_size = batch_size
+            steps_per_epoch = len(train_loader)
+            throughput = int(batch_size * steps_per_epoch /
+                             (time.time() - tic))
+            logger.info('[Epoch %d] training: loss=%f, top1=%f, top5=%f' %
+                        (epoch + 1, losses_m, top1_m, top5_m))
+            logger.info('[Epoch %d] speed: %d samples/sec\ttime cost: %f',
+                        epoch + 1, throughput,
+                        time.time() - tic)
 
             if not skip_validation:
                 prec1 = validate(
@@ -305,15 +335,12 @@ def train_loop(
                     use_amp=use_amp,
                 )
                 if use_ema:
-                    model_ema.load_state_dict({k.replace('module.', ''): v for k, v in ema.state_dict().items()})
-                    prec1 = validate(
-                        val_loader,
-                        criterion,
-                        model_ema,
-                        num_class,
-                        logger,
-                        "Val-log"
-                    )
+                    model_ema.load_state_dict({
+                        k.replace('module.', ''): v
+                        for k, v in ema.state_dict().items()
+                    })
+                    prec1 = validate(val_loader, criterion, model_ema,
+                                     num_class, logger, "Val-log")
 
                 if prec1 > best_prec1:
                     is_best = True
@@ -324,9 +351,8 @@ def train_loop(
                 is_best = True
                 best_prec1 = 0
 
-            if save_checkpoints and (
-                not torch.distributed.is_initialized() or torch.distributed.get_rank() == 0
-            ):
+            if save_checkpoints and (not torch.distributed.is_initialized()
+                                     or torch.distributed.get_rank() == 0):
                 checkpoint_state = {
                     "epoch": epoch + 1,
                     "state_dict": model.state_dict(),
